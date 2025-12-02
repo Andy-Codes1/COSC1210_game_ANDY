c0e0e22bdf19344621e6c168a89552ec7eedcff2
Andy Vo <AndyVo5817@gmail.com>
2025-12-02 17:02:21 -0600

    UPDATE

--- Minesweeper_Current/Classes.py
+++ Minesweeper_Current/Classes.py
@@ -1,21 +1,8 @@
-class Board: 
-    def __init__(self):
-        self.board_surface = pygame.Surface(WIDTH, HEIGHT)
-        self.dug = [] #empty list for dug bombs
+from Settings import * 
+#the * is a wildcard import, so I can directly mention all files without having to put Settings._ in front of it.
+import random
+import pygame 
 
-
-
-    def place_clues():
-        '''So far only have the logic for if it's not a bomb'''
-        for x in GRID_ROWS: 
-            for y in GRID_COLS: 
-                if self.board[x][y].type != 'B':
-                #B refers to the TYPE of Bombs written in ADVENTURE FUNCTIONS. 
-                    total_bombs = self.bombs_near(x,y)
-                    if total_bombs > 1:
-                        self.board.image
-
-#mine_location_dictionary(Total_bombs)
 
 # Tile Types for 2D board
 # "B" = Bomb
@@ -26,8 +13,8 @@ class Tile:
     # so basically I am making a class for Tiles, x coordinate, y cordinate, and the type as parameter. I'm making revealed and flagged as false because they are not true yet
     def __init__(self, x, y, image, type, revealed=False, flagged=False):
         #now I am setting self.x and y to a box which is why it's 70/8 multiplied by the x value 
-        self.x = x * (70 / 8)
-        self.y = y * (70 / 8)
+        self.x = x * TILE_SIZE
+        self.y = y * TILE_SIZE
         self.image = image
         self.type = type
         self.revealed = revealed 
@@ -41,12 +28,74 @@ class Tile:
         elif not self.revealed:
             board_surface.blit(self.unknown, (self.x, self.y))
 
+def __repr__(self):
+    return self.type
 
-#def mine_location_dictionary(Total_bombs):
- #   Location_Bombs = {}
- #   for ix in GRID_ROWS:
- #       for iy in GRID_COLS:
- #           if Bombs.type == "B":
-  #              Location_Bombs.append(ix, iy)
- #   print(Location_Bombs)
-    #To see a 2D model of all bombs and their locations. 
+class Board:
+    def __init__(self):
+        self.board_surface = pygame.Surface((WIDTH, HEIGHT))
+        self.board_list = [[Tile(GRID_COLS, GRID_ROWS, tile_empty, "X") for row in range(GRID_ROWS)] for col in range(GRID_COLS)]
+        self.place_mines()
+        self.place_clues()
+        self.dug = []
+    def place_mines(self):
+        for i in range(MINES):
+            while True:
+                x = random.randint(0, GRID_ROWS-1)
+                y = random.randint(0, GRID_COLS-1)
+                if self.board_list[x][y].type == "x":
+                    self.board_list[x][y].image = tile_mine 
+    
+    def place_clues(self):
+        for x in range(GRID_ROWS):
+            for y in range(GRID_COLS):
+                if self.board_list[x][y].type != "B":
+                    MINES = self.check_neighbours(x,y)
+                    if MINES > 0:
+                        self.board_list[x][y].image = tile_numbers[MINES-1]
+                        self.board_list[x][y].type = "C"
+
+    @staticmethod
+    def inside(x,y):
+        return 0 <= x < GRID_ROWS and 0 <= y < GRID_COLS
+    
+
+    # To check for neighbouring bombs, the code will take in an x and y for a square, bombs nearby will be initalized to 0, then 
+    # then we will check toward the left of the bomb and right of the bomb thus the starting at -1, and ending before 2, and top and down 
+    # if the bomb is inside the 8x8 map and the square is a bomb then the square will add one bomb to its clue. after, we return that final number
+    def check_neighbours(self, x, y):
+        MINES = 0 
+        for x_offset in range(-1,2):
+            for y_offset in range(-1,2):
+                neighbour_x = x + x_offset
+                neighbour_y = y + y_offset
+                if self.inside(neighbour_x, neighbour_y) and self.board_list[neighbour_x][neighbour_y].type == "B":
+                    MINES +=1 
+        return MINES
+
+    def draw(self, screen):
+        for row in self.board_list:
+            for tile in row:
+                tile.draw(self.board_surface)
+        screen.blit(self.board_surface, (0,0))
+    def dig(self, x, y):
+        self.dug.append((x, y))
+        if self.board_list[x][y].type == "X":
+            self.board_list[x][y].revealed = True
+            self.board_list[x][y].image = tile_exploded
+            return False
+        elif self.board_list[x][y].type == "C":
+            self.board_list[x][y].revealed = True
+            return True
+
+        self.board_list[x][y].revealed = True
+
+        for row in range(max(0, x-1), min(GRID_ROWS-1, x+1) + 1):
+            for col in range(max(0, y-1), min(GRID_COLS-1, y+1) + 1):
+                if (row, col) not in self.dug:
+                    self.dig(row, col)
+        return True
+
+    def display_board(self):
+        for row in self.board_list:
+            print(row)
--- Minesweeper_Current/Settings.py
+++ Minesweeper_Current/Settings.py
@@ -1,16 +1,17 @@
-# config.py
+import pygame
+import os 
+# OS is used to interact with operating system and work around directories
 
 # Grid and Room Settings
+# Number is Pixels
+TILE_SIZE = 64 
 GRID_ROWS = 8
 GRID_COLS = 8
-ROOM_WIDTH = 70
-ROOM_HEIGHT = 70
+ROOM_WIDTH = TILE_SIZE * GRID_ROWS
+ROOM_HEIGHT = TILE_SIZE * GRID_COLS
 WALL_THICK = 4
-DOOR_SIZE = 0
-
-# Player Settings
-PLAYER_RADIUS = 10
-PLAYER_SPEED = 100
+MINES = 10
+TITLE = "MINESWEEPER"
 
 # Screen Size
 WIDTH = ROOM_WIDTH * GRID_COLS
@@ -25,7 +26,26 @@ BOX_COLOR = (225,153,100)
 # Frames Per Second
 FPS = 60
 
-# --- Player appearance ---
-PLAYER_IMAGE_PATH = "Assets/bomb.png"  # transparent PNG, e.g. 48x48
-PLAYER_IMAGE_SIZE = (74, 74)             # w, h in pixels
-PLAYER_RADIUS = 24                       # half of width; used by your collision
+BASE_DIR = os.path.dirname(__file__)
+ASSETS_DIR = os.path.join(BASE_DIR, "Assets")
+
+# initialize pygame (needed before some image/transform ops)
+pygame.init()
+
+# Intialize empty list of tile numbers starting at 1 and ending before 9
+# Then use a for loop to append all tile pngs into the game all at once
+tile_numbers = []
+for i in range(1,9):
+    path = os.path.join(ASSETS_DIR, f"Clue{i}.png")
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Missing asset: {path}")
+    tile_numbers.append(pygame.transform.scale(pygame.image.load(path), (TILE_SIZE, TILE_SIZE)))
+
+
+# Using transfrom and scale from pygame to load in correctly sized images
+tile_empty = pygame.transform.scale(pygame.image.load(os.path.join(ASSETS_DIR, "tile_5.png")), (TILE_SIZE, TILE_SIZE))
+tile_exploded = pygame.transform.scale(pygame.image.load(os.path.join(ASSETS_DIR, "tile_38.png")), (TILE_SIZE, TILE_SIZE))
+tile_flag = pygame.transform.scale(pygame.image.load(os.path.join(ASSETS_DIR, "flagged.png")), (TILE_SIZE, TILE_SIZE))
+tile_mine = pygame.transform.scale(pygame.image.load(os.path.join(ASSETS_DIR, "bomb.png")), (TILE_SIZE, TILE_SIZE))
+tile_unknown = pygame.transform.scale(pygame.image.load(os.path.join(ASSETS_DIR, "Brick1.png")), (TILE_SIZE, TILE_SIZE))
+tile_not_mine = pygame.transform.scale(pygame.image.load(os.path.join(ASSETS_DIR, "notbomb.png")), (TILE_SIZE, TILE_SIZE))
